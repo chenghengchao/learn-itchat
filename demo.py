d9e89dd7a158e9e631752553e5ee18dc03cc93e9
chenghengchao <2511644508@qq.com>
2017-04-11 15:59:53 +0800

    LBS

---
+++ demo.py
@@ -0,0 +1,23 @@
+import json
+import requests
+import pprint
+
+# key = 'tLdYoFGg7SFPC1LKRxDO8DQqyC5amuBi'
+key = 'IwKTtP7vQ5Mahn5dykDACKqKdzFUY2uT'
+city = '北京'
+url = 'http://api.map.baidu.com/telematics/v3/weather?location={}&output=json&ak={}'.format(city, key)
+# url = 'http://api.map.baidu.com/telematics/v2/search?q={}&region={}&output=json&ak={}'.format('饭店', city, key)
+App_secret = '7a9676bb8ceea89685eaeb9ec4c7fb4b'
+AppID = 'wx92a5fb372a254be2'
+
+print(url)
+response = requests.get(url)
+weather_dict = response.json()
+# pprint.pprint(weather_dict)
+print(weather_dict['results'][0]['currentCity'])
+weather_data = weather_dict['results'][0]['weather_data']
+for item in weather_data:
+    print(item['date'])
+    print(item['weather'])
+    print(item['wind'])
+    print(item['temperature'])
